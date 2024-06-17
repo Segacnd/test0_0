1a4782cb588632707b6c505cfb192edd16e85c3f
Sega <Segabelka@yandex.ru>
2024-06-17 12:23:43 +0300

    nothing 0_0

---
+++ src/assets/mock/data.ts
@@ -0,0 +1,30 @@
+const mockArray = [
+	{
+		id: 934,
+		duration: '0.25',
+		title: 'asdasd',
+		task_description: '',
+		date: '2024-03-02',
+	},
+	{
+		id: 11,
+		duration: '3.25',
+		title: 'asdasd',
+		task_description: '',
+		date: '2024-01-02',
+	},
+	{
+		id: 145,
+		duration: '2.00',
+		title: 'asdasd',
+		task_description: '',
+		date: '2024-02-02',
+	},
+	{
+		id: 520,
+		duration: '8.00',
+		title: 'gyujgkjyhg',
+		task_description: '',
+		date: '2024-05-06',
+	},
+];
